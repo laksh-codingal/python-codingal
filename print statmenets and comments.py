837d9729c168389a721a27071062708cabc32cbe
laksh-codingal <lakshadit.varun@gmail.com>
2025-04-17 11:08:06 +0530

    lesson1

---
+++ print statmenets and comments.py
@@ -0,0 +1,9 @@
+#printing a greeting message
+
+print("Hello everyone, good evening!")
+#printing my name
+print("My name is Laksh")
+#printing a fun fact about myself 
+print("Laksh loves to program and code")
+#performing and printing a simple calculation
+print("3 + 9 is ", 3 + 9 )
