c32cf8e4c19c321b8437a948ab6aad13562ff9c4
laksh-codingal <lakshadit.varun@gmail.com>
2025-04-22 10:37:34 +0530

    lesson2

---
+++ lesson2/keywords.py
@@ -0,0 +1,7 @@
+
+#print all the keywords in python 
+
+import keyword
+
+print("The python keywords are: \n")
+print(keyword.kwlist)
---
+++ lesson2/welcome to the world of coding.py
@@ -0,0 +1,20 @@
+
+#print a sentence
+
+print("My name is Laksh")
+
+#print a number
+
+print(7770899)
+
+#next line after print
+
+print("I love coding \n")
+
+#pass multiple arguments for printing 
+
+print("I dance every sunday", 7708, "my favorite number is 7")
+
+#end argument of print statement
+
+print("I like to do origami ", end= "Thank you")
