f1ad2e937db9d9cef450dee3dd52d111ec179753
laksh-codingal <lakshadit.varun@gmail.com>
2025-04-22 10:55:58 +0530

    storing birthdays after class project

---
+++ after_class_projects/storing_birthdays.py
@@ -0,0 +1,17 @@
+print("5 friend's birthdays")
+
+#stroing birthdays 
+
+amruth_birthday = "amruth birthday is on monday"
+print("\n", amruth_birthday)
+anushka_birthday = "anushka birthday is on tuesday"
+print("\n", anushka_birthday)
+laksh_birthday = "laksh birthday is on wednesday"
+print("\n", laksh_birthday)
+affan_birthday = "affan birthday is on thursday"
+print("\n", affan_birthday)
+arjun_birthday = "arjun birthday is on friday"
+print("\n", arjun_birthday)
+print("\n thank you ")
+print("\n")
+
