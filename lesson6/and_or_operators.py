c98758a8f69595cb54ce7525e204daf69fde3eaa
laksh-codingal <lakshadit.varun@gmail.com>
2025-05-08 10:57:21 +0530

    lesson6

---
+++ lesson6/and_or_operators.py
@@ -0,0 +1,24 @@
+
+a =  10 
+b = 9
+c = 0 
+
+if a and b and c: 
+    print("all the numbers have boolean value as true")
+else:
+    print("atleast one number has boolean value as false")
+
+a2 = 100
+b2 = -10
+c2 = 0 
+
+if a2>0 or b2>0:
+    print("both the number are greater than zero")
+else:
+    print("no number is greater than zero")
+
+if b2>0 or c2>0:
+    print("both the numbers are greater than zero ")
+else:
+    print("no number is greater than zero")
+    
---
+++ lesson6/not_operator.py
@@ -0,0 +1,24 @@
+a = 5
+b = 27
+c = 5
+ 
+print(a!=b)
+print(b!=c)
+
+
+a2 = "games"
+b2 = "coding"
+
+if a2!=b2:
+    print("both a and b are different")
+
+a3 = 99
+b3 = 98
+
+if(a3 == 1)!=(b3 == 98):
+    print("hello")
+
+a4 = int(input("enter a number:- "))
+if a4%2 != 0:
+    print("it is not an even number")
+
