cf6ce8af3e7484c709a69f4dc762176c3d57a2f2
laksh-codingal <lakshadit.varun@gmail.com>
2025-06-03 21:11:48 +0530

    lesson13

---
+++ lesson13/traingle_stars.py
@@ -0,0 +1,13 @@
+
+print("triangle stars ")
+
+n = int(input("enter the number of rows:- "))
+
+for i in range(n):
+
+    for j in range(i+1):
+
+        print("*", end= " ")
+    
+    print( )
+    
