7a3e459d19c852a8eda2cfc022807bf839c1b692
laksh-codingal <lakshadit.varun@gmail.com>
2025-06-22 21:19:25 +0530

    trigonometric.py

---
+++ after_class_projects/trigonometric.py
@@ -0,0 +1,10 @@
+
+import math
+
+x = int(input("enter a number:- "))
+
+print(f"The sin value of {x} is", math.sin(x))
+print(f"The tan value of {x} is", math.tan(x))
+print(f"The cos value of {x} is", math.cos(x))
+
+
