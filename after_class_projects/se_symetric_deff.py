4abae2b81b32c9ef225dc4465690aac292d86688
laksh-codingal <lakshadit.varun@gmail.com>
2025-07-19 14:36:02 +0530

    set_symetric_diff

---
+++ after_class_projects/se_symetric_deff.py
@@ -0,0 +1,13 @@
+
+def symmetric_difference(set1, set2):
+    return set1.symmetric_difference(set2)
+
+
+A = {1, 2, 3}
+B = {3, 4, 5}
+
+
+result = symmetric_difference(A, B)
+
+
+print("Symmetric Difference:", result)
