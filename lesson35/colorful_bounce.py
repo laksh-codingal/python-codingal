b3c5b55dae90f6ad5a982579d0a081907b4c622b
laksh-codingal <lakshadit.varun@gmail.com>
2025-11-28 19:54:11 +0530

    lesson35

---
+++ lesson35/colorful_bounce.py
@@ -0,0 +1,81 @@
+
+import pygame as p
+import random
+
+p.init()
+
+SPRITE_COLOR_CHANGE_EVENT = p.USEREVENT + 1
+BACKGROUND_COLOR_CHANGE_EVENT = p.USEREVENT + 2
+
+BLUE = p.Color("blue")
+LIGHTBLUE = p.Color("lightblue")
+DARKBLUE = p.Color("darkblue")
+
+YELLOW = p.Color("yellow")
+MAGENTA = p.Color("magenta")
+RED = p.Color("red")
+GREEN = p.Color("green")
+
+class Sprite(p.sprite.Sprite):
+    def __init__(self, color, height, width):
+        super().__init__()
+        self.image = p.Surface([width, height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.velocity = [random.choice([-1, 1]),  random.choice([-1, 1])]
+    def update(self):
+        self.rect.move_ip(self.velocity)
+        boundary_hit = False
+        if self.rect.left <= 0 or self.rect.right >= 500:
+            self.velocity[0] = -self.velocity[0]
+            boundary_hit = True
+        
+        if self.rect.top <= 0 or self.rect.bottom >= 400:
+            self.velocity[1] = -self.velocity[1]
+            boundary_hit = True
+
+        if boundary_hit:
+            p.event.post(p.event.Event(SPRITE_COLOR_CHANGE_EVENT))
+
+            p.event.post(p.event.Event(BACKGROUND_COLOR_CHANGE_EVENT))
+
+    def change_color(self):
+        self.image.fill(random.choice([YELLOW, MAGENTA, RED, GREEN]))
+
+def change_background_color():
+    global bg_color
+    bg_color = random.choice([BLUE, LIGHTBLUE, DARKBLUE])
+
+
+all_sprites_list = p.sprite.Group()
+sp1 = Sprite(MAGENTA, 20, 30)
+sp1.rect.x = random.randint(0, 480)
+sp1.rect.y = random.randint(0,370)
+all_sprites_list.add(sp1)
+screen = p.display.set_mode((500, 400))
+p.display.set_caption("colorful bounce")
+bg_color = BLUE
+screen.fill(bg_color)
+
+exit = False
+clock = p.time.Clock()
+while not exit:
+    for event in p.event.get():
+        if event.type == p.QUIT:
+            exit = True
+        elif event.type == SPRITE_COLOR_CHANGE_EVENT:
+            sp1.change_color()
+        elif event.type == BACKGROUND_COLOR_CHANGE_EVENT:
+            change_background_color()
+
+    all_sprites_list.update()
+    screen.fill(bg_color)
+    all_sprites_list.draw(screen)
+    p.display.flip()
+    clock.tick(240)
+p.quit()
+
+
+        
+
+    
